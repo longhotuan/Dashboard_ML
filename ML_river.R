c7f717b45d9f00e69e1b559dec347d85adc9fa88
longhotuan <hotuanlong87@gmail.com>
2020-10-02 10:30:00 +0200

    First commit

---
+++ ML_river.R
@@ -0,0 +1,24 @@
+#### Global R ####
+
+library(knitr)
+library(rmarkdown)
+library(tidyverse)
+library(reshape)
+library(plotly)
+library(flexdashboard)
+library(shiny)
+library(data.table)
+library(lubridate)
+library(shinythemes)
+library(shinydashboard)
+library(tweenr)
+library(leaflet)
+library(leaflet.minicharts)
+library(RColorBrewer)
+library(rsconnect)
+library(DT)
+library(usethis)
+library(feather)
+
+river_ml <- read_feather("river_ml.feather")
+
